2042feb7227c954ae23eab7bb4519d2e075f2e7a
Milen Radkov <milenradkov@me.com>
2022-05-25 21:55:25 +0300

    x

--- index.js
+++ index.js
@@ -2,8 +2,5 @@ const GasTable = require('./lib/gasTable');
 const data = require('./gasReporterOutput.json');
 const data2 = require('./gasReporterOutput2.json');
 
-console.log(JSON.stringify(data));
-console.log(JSON.stringify(data2));
-
 const table = new GasTable(data.config);
 table.generate(data.info, data2.info);
